c880922840a2a0e3f8f8ee524601fb1b2e2cf729
Jorge Bernal <koke@amedias.org>
2011-04-08 13:58:54 +0200

    Initial Commit

---
+++ IDNTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  IDNTest
+//
+//  Created by Jorge Bernal on 4/8/11.
+//  Copyright 2011 Automattic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
