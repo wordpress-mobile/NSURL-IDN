a1bd9218fe6283fac9322a962f430378ae3e85cf
Jorge Bernal <koke@amedias.org>
2011-04-08 14:37:21 +0200

    Added NSURL+IDN extensions plus a small test suite

--- IDNTest/main.m
+++ IDNTest/main.m
@@ -7,14 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NSURL+IDN.h"
+
+void testIDN(NSString *decoded, NSString *expected) {
+    NSString *encoded = [NSURL IDNEncodedHostname:decoded];
+    if ([encoded isEqualToString:expected])
+        NSLog(@"%@ -> %@", decoded, encoded);
+    else
+        NSLog(@"%@ -> %@ (Expected: %@)", decoded, encoded, expected);
+}
 
 int main (int argc, const char * argv[])
 {
-
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+    testIDN(@"http://test.soymaño.com", @"http://test.xn--soymao-0wa.com");
+    testIDN(@"إختبار", @"xn--kgbechtv");
+    testIDN(@"испытание", @"xn--80akhbyknj4f");
 
     [pool drain];
     return 0;
